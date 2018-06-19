4afc26081bf80d51fc385ee7f97365becfe67444
Andreas Greimel <andreas.greimel@tngtech.com>
2018-06-19 17:10:22 +0200

    Publish markers in hand_publisher, improve movement
    This allows to visualize the movement in RViz

--- demo_nodes/hand_publisher.cpp
+++ demo_nodes/hand_publisher.cpp
@@ -11,15 +11,17 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rcutils/cmdline_parser.h"
 #include "leap_msgs/msg/leap_frame.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
 
 using namespace std::chrono_literals;
 
 class HandPublisher : public rclcpp::Node {
 public:
   HandPublisher(std::string topic, std::string frame)
-  : Node("hand_publisher"), frame_(frame), counter_(0), period_(50), time_lapse_(100ms)
+      : Node("hand_publisher"), frame_(frame), counter_(0), period_(20), time_lapse_(100ms) 
   {
     publisher_ = create_publisher<leap_msgs::msg::LeapFrame>(topic);
+    marker_publisher_ = create_publisher<visualization_msgs::msg::MarkerArray>(topic + "_markers");
 
     timer_ = this->create_wall_timer(time_lapse_, [this]() -> void {
       publish_hands();
@@ -30,11 +32,13 @@ private:
   void publish_hands()
   {
     std::cout << "publishing hands..." << std::endl;
-    auto message = create_msg((++counter_ % period_) * 0.005);
+    auto message = create_msg((++counter_ % period_) * 1.0 / period_ * M_PI * 2);
+    auto marker_message = create_markers_from_leap_frame(message);
     publisher_->publish(message);
+    marker_publisher_->publish(marker_message);
   }
 
-  leap_msgs::msg::HandArray create_msg(double elongation)
+  leap_msgs::msg::LeapFrame create_msg(double elongation)
   {
     auto message = leap_msgs::msg::LeapFrame();
     message.header = std_msgs::msg::Header();
@@ -43,17 +47,60 @@ private:
 
     leap_msgs::msg::Hand left, right;
 
-    left.position.z = 0; right.position.z = 0;
-    left.position.y = 0.02; right.position.y = 0.02;
-    left.position.x =  +elongation; right.position.x = -elongation;
+    left.position.z = 0;
+    right.position.z = 0;
+    left.position.y = 0.02;
+    right.position.y = 0.02;
+    left.position.x = (std::sin(elongation)+1) / 4;
+    right.position.x = (-std::sin(elongation)-1) / 4;
 
-    left.orientation.w = 0; right.orientation.w = M_PI;
-    left.orientation.x = 0; right.orientation.x = 0;
-    left.orientation.y = 1; right.orientation.y = 1;
-    left.orientation.z = 0; right.orientation.z = 0;
+    left.orientation.w = 0;
+    right.orientation.w = M_PI;
+    left.orientation.x = 0;
+    right.orientation.x = 0;
+    left.orientation.y = 1;
+    right.orientation.y = 1;
+    left.orientation.z = 0;
+    right.orientation.z = 0;
 
     message.hands.push_back(left);
     message.hands.push_back(right);
+    return message;
+  }
+
+  visualization_msgs::msg::MarkerArray create_markers_from_leap_frame(leap_msgs::msg::LeapFrame leap_frame)
+  {
+    auto message = visualization_msgs::msg::MarkerArray();
+
+    int id = 0;
+    for (const auto &hand : leap_frame.hands)
+    {
+      auto hand_marker = visualization_msgs::msg::Marker();
+      hand_marker.header = std_msgs::msg::Header();
+      hand_marker.header.frame_id = frame_;
+      hand_marker.header.stamp = leap_frame.header.stamp;
+
+      hand_marker.id = ++id;
+      hand_marker.ns = "hand_markers";
+      hand_marker.type = visualization_msgs::msg::Marker::SPHERE;
+      hand_marker.action = visualization_msgs::msg::Marker::ADD;
+      hand_marker.lifetime = rclcpp::Duration(1, 0);
+
+      hand_marker.scale.x = 0.1;
+      hand_marker.scale.y = 0.1;
+      hand_marker.scale.z = 0.1;
+
+      hand_marker.color.a = 1.0;
+      hand_marker.color.r = 1.0;
+      hand_marker.color.g = 0.0;
+      hand_marker.color.b = 0.0;
+
+      hand_marker.pose.position = hand.position;
+      hand_marker.pose.orientation = hand.orientation;
+
+      message.markers.emplace_back(hand_marker);
+    }
+
     return message;
   }
 
@@ -63,16 +110,19 @@ private:
   std::chrono::milliseconds time_lapse_;
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<leap_msgs::msg::LeapFrame>::SharedPtr publisher_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_publisher_;
 };
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
   std::string topic = "hands";
-  if (rcutils_cli_option_exist(argv, argv + argc, "-t")) {
+  if (rcutils_cli_option_exist(argv, argv + argc, "-t"))
+  {
     topic = std::string(rcutils_cli_get_option(argv, argv + argc, "-t"));
   }
   std::string frame = "leap_frame";
-  if (rcutils_cli_option_exist(argv, argv + argc, "-f")) {
+  if (rcutils_cli_option_exist(argv, argv + argc, "-f"))
+  {
     frame = std::string(rcutils_cli_get_option(argv, argv + argc, "-f"));
   }
 
