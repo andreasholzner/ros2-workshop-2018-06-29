52e365ceacdd62441de923c50e385fb1bbabc44f
Philip Z <philipzimmermann@ymail.com>
2018-06-29 13:31:11 +0200

    added new message type

--- subscriber/SubscriberNode.cpp
+++ subscriber/SubscriberNode.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include "messages/msg/custom_message.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "rclcpp/rclcpp.hpp"
 
@@ -12,8 +13,12 @@ public:
         subscription = create_subscription<std_msgs::msg::String>("first_demo", [] (std::shared_ptr<std_msgs::msg::String> msg) {
             std::cout << msg->data << std::endl;
         });
+        customSubscription = create_subscription<messages::msg::CustomMessage>("first_demo", [] (std::shared_ptr<messages::msg::CustomMessage> msg) {
+                    std::cout << msg->foo << std::endl;
+        });
     }
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription;
+    rclcpp::Subscription<messages::msg::CustomMessage>::SharedPtr customSubscription;
 };
 
 
